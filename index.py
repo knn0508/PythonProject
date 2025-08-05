1a48dd25bf4edbcf76cc6d64532734e9dba4c448
knn0508 <kanan.salmanli.i@student.aztu.edu.az>
2025-08-05 16:46:59 +0400

    commit "Update configuration and add deployment files"

--- index.py
+++ index.py
@@ -1,12 +1,7 @@
-from app import app
+from simple_app import app
 
 # This is the entry point for Vercel
-def handler(event, context):
-    return app
+app = app
 
-# Export the app for Vercel
-application = app
-
-# For local development
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run()
